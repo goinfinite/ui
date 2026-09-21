b60eafae32d46fda8437606efc7e5a4901948f56
ntorga <northontorga@gmail.com>
2026-09-21 19:30:10 -0300

    feat: add the data table component

---
+++ src/structural/dataTable.go
@@ -0,0 +1,283 @@
+package uiStructural
+
+import (
+	"fmt"
+	"hash/fnv"
+	"maps"
+
+	"github.com/a-h/templ"
+	uiForm "github.com/goinfinite/ui/src/form"
+)
+
+type DataTableDensity string
+
+const (
+	DataTableDensityComfortable DataTableDensity = "comfortable"
+	DataTableDensityDense       DataTableDensity = "dense"
+)
+
+type DataTableAlignment string
+
+const (
+	DataTableAlignmentLeft   DataTableAlignment = "left"
+	DataTableAlignmentCenter DataTableAlignment = "center"
+	DataTableAlignmentRight  DataTableAlignment = "right"
+)
+
+func (alignment DataTableAlignment) alignmentClassResolver() string {
+	switch alignment {
+	case DataTableAlignmentCenter:
+		return "text-center"
+	case DataTableAlignmentRight:
+		return "text-right"
+	}
+	return "text-left"
+}
+
+func (alignment DataTableAlignment) justifyClassResolver() string {
+	switch alignment {
+	case DataTableAlignmentCenter:
+		return "justify-center"
+	case DataTableAlignmentRight:
+		return "justify-end"
+	}
+	return "justify-start"
+}
+
+type DataTableSortDirection string
+
+const (
+	DataTableSortDirectionAsc  DataTableSortDirection = "asc"
+	DataTableSortDirectionDesc DataTableSortDirection = "desc"
+)
+
+type DataTableHeaderTextCase string
+
+const (
+	DataTableHeaderTextCaseLower DataTableHeaderTextCase = "lower"
+	DataTableHeaderTextCaseUpper DataTableHeaderTextCase = "upper"
+)
+
+type DataTablePageSize uint
+
+const (
+	DataTableUrlPlaceholderPageNumber    string = "{pageNumber}"
+	DataTableUrlPlaceholderItemsPerPage  string = "{itemsPerPage}"
+	DataTableUrlPlaceholderSortKey       string = "{sortKey}"
+	DataTableUrlPlaceholderSortDirection string = "{sortDirection}"
+	DataTableUrlPlaceholderSearch        string = "{search}"
+
+	dataTableDefaultPaginationAriaLabel string = "Table pagination"
+	dataTableDefaultRefreshDebounceMs   uint   = 300
+)
+
+type DataTableColumnSettings[Row any] struct {
+	Label        string
+	CellRenderer func(row Row) templ.Component
+
+	// OptionalFields
+	Alignment     DataTableAlignment
+	CellClass     string
+	MaxWidthClass string
+	MinWidthClass string
+	SortKey       string
+	WidthPercent  uint8
+}
+
+type DataTableSettings[Row any] struct {
+	Columns []DataTableColumnSettings[Row]
+	Rows    []Row
+
+	// OptionalFields
+	BulkActions             templ.Component
+	CheckboxCheckedColor    string
+	CheckboxShape           string
+	CheckboxSize            string
+	CheckboxUncheckedColor  string
+	Density                 DataTableDensity
+	EmptyState              templ.Component
+	Filters                 []FilterSettings
+	HeaderActions           templ.Component
+	HeaderClass             string
+	HeaderTextCase          DataTableHeaderTextCase
+	Id                      string
+	InitialFilterValues     map[string]any
+	InitialSearchQuery      string
+	InitialSortDirection    DataTableSortDirection
+	InitialSortKey          string
+	IsHeaderSticky          bool
+	IsStriped               bool
+	ItemsPerPage            DataTablePageSize
+	ItemsPerPageSizeChoices []DataTablePageSize
+	ItemsTotal              uint
+	PageNumber              uint
+	PaginationAriaLabel     string
+	PagesTotal              uint
+	QueryUrlTemplate        string
+	RefreshDebounceMs       uint
+	RefreshOnEvents         []string
+	RowClassResolver        func(row Row) string
+	RowIdResolver           func(row Row) string
+	RowLabelResolver        func(row Row) string
+	SearchBox               templ.Component
+	SearchBoxAlignment      DataTableAlignment
+}
+
+type dataTableInitialState struct {
+	FilterValues  map[string]any `json:"filterValues"`
+	ItemsPerPage  uint           `json:"itemsPerPage"`
+	PageNumber    uint           `json:"pageNumber"`
+	SearchQuery   string         `json:"searchQuery"`
+	SortDirection string         `json:"sortDirection"`
+	SortKey       string         `json:"sortKey"`
+}
+
+type dataTableClientSettings struct {
+	FilterQueryParamNames map[string]string     `json:"filterQueryParamNames"`
+	InitialState          dataTableInitialState `json:"initialState"`
+	RefreshDebounceMs     uint                  `json:"refreshDebounceMs"`
+	RefreshOnEvents       []string              `json:"refreshOnEvents"`
+	QueryUrlTemplate      string                `json:"queryUrlTemplate"`
+}
+
+func (settings DataTableSettings[Row]) initialFilterValuesResolver() map[string]any {
+	initialValues := map[string]any{}
+	for _, filter := range settings.Filters {
+		switch filter.Kind {
+		case FilterKindNumberRange, FilterKindDateRange:
+			initialValues[filter.Key] = map[string]string{"min": "", "max": ""}
+		default:
+			initialValues[filter.Key] = ""
+		}
+	}
+	maps.Copy(initialValues, settings.InitialFilterValues)
+	return initialValues
+}
+
+func (settings DataTableSettings[Row]) clientSettingsResolver(
+	itemsPerPage, pageNumber uint,
+) dataTableClientSettings {
+	filterQueryParamNames := map[string]string{}
+	for _, filter := range settings.Filters {
+		queryParamName := filter.QueryParamName
+		if queryParamName == "" {
+			queryParamName = filter.Key
+		}
+		filterQueryParamNames[filter.Key] = queryParamName
+	}
+	refreshDebounceMs := settings.RefreshDebounceMs
+	if refreshDebounceMs == 0 {
+		refreshDebounceMs = dataTableDefaultRefreshDebounceMs
+	}
+	return dataTableClientSettings{
+		FilterQueryParamNames: filterQueryParamNames,
+		InitialState: dataTableInitialState{
+			FilterValues:  settings.initialFilterValuesResolver(),
+			ItemsPerPage:  itemsPerPage,
+			PageNumber:    pageNumber,
+			SearchQuery:   settings.InitialSearchQuery,
+			SortDirection: string(settings.InitialSortDirection),
+			SortKey:       settings.InitialSortKey,
+		},
+		RefreshDebounceMs: refreshDebounceMs,
+		RefreshOnEvents:   settings.RefreshOnEvents,
+		QueryUrlTemplate:  settings.QueryUrlTemplate,
+	}
+}
+
+func (settings DataTableSettings[Row]) tableIdentityHashResolver() uint64 {
+	hasher := fnv.New64a()
+	hasher.Write([]byte(settings.QueryUrlTemplate))
+	for _, column := range settings.Columns {
+		hasher.Write([]byte(column.Label))
+		hasher.Write([]byte(column.SortKey))
+	}
+	for _, filter := range settings.Filters {
+		hasher.Write([]byte(filter.Key))
+		hasher.Write([]byte(filter.QueryParamName))
+	}
+	return hasher.Sum64()
+}
+
+func (settings DataTableSettings[Row]) idResolver() string {
+	if settings.Id != "" {
+		return settings.Id
+	}
+	return fmt.Sprintf("dataTable-%x", settings.tableIdentityHashResolver())
+}
+
+func (settings DataTableSettings[Row]) itemsPerPageSizeChoicesResolver() []uint {
+	if len(settings.ItemsPerPageSizeChoices) == 0 {
+		return paginationDefaultItemsPerPageSizeChoices
+	}
+	itemsPerPageSizeChoices := make([]uint, len(settings.ItemsPerPageSizeChoices))
+	for index, pageSize := range settings.ItemsPerPageSizeChoices {
+		itemsPerPageSizeChoices[index] = uint(pageSize)
+	}
+	return itemsPerPageSizeChoices
+}
+
+func (settings DataTableSettings[Row]) itemsPerPageResolver(
+	itemsPerPageSizeChoices []uint,
+) uint {
+	if settings.ItemsPerPage > 0 {
+		return uint(settings.ItemsPerPage)
+	}
+	return itemsPerPageSizeChoices[0]
+}
+
+func (settings DataTableSettings[Row]) pageNumberResolver() uint {
+	if settings.PageNumber > 0 {
+		return settings.PageNumber
+	}
+	return 1
+}
+
+func (settings DataTableSettings[Row]) paginationAriaLabelResolver() string {
+	if settings.PaginationAriaLabel != "" {
+		return settings.PaginationAriaLabel
+	}
+	return dataTableDefaultPaginationAriaLabel
+}
+
+func (settings DataTableSettings[Row]) headerTextCaseClassResolver() string {
+	if settings.HeaderTextCase == DataTableHeaderTextCaseUpper {
+		return "uppercase"
+	}
+	return "lowercase"
+}
+
+func (settings DataTableSettings[Row]) cellPaddingClassesResolver() string {
+	if settings.Density == DataTableDensityDense {
+		return "px-2 py-1.5"
+	}
+	return "px-3 py-3"
+}
+
+func (settings DataTableSettings[Row]) headerPaddingClassesResolver() string {
+	if settings.Density == DataTableDensityDense {
+		return "px-2 py-1.5"
+	}
+	return "px-3 py-2"
+}
+
+func (settings DataTableSettings[Row]) checkboxShapeResolver() string {
+	if settings.CheckboxShape != "" {
+		return settings.CheckboxShape
+	}
+	return uiForm.CheckboxInputShapeSquare
+}
+
+func (settings DataTableSettings[Row]) checkboxSizeResolver() string {
+	if settings.CheckboxSize != "" {
+		return settings.CheckboxSize
+	}
+	return uiForm.CheckboxInputSizeMd
+}
+
+func (settings DataTableSettings[Row]) rowStripeClassesResolver() string {
+	if settings.IsStriped {
+		return "odd:bg-neutral-50/5 odd:hover:bg-neutral-50/10"
+	}
+	return ""
+}
---
+++ src/structural/dataTableState.js
@@ -0,0 +1,334 @@
+UiToolset.RegisterAlpineState(() => {
+  const unsortedSortIconClass =
+    "ph-caret-up-down opacity-0 group-hover:opacity-40 group-focus-visible:opacity-40";
+
+  Alpine.data("dataTable", (settingsScriptId) => ({
+    queryUrlTemplate: "",
+    refreshDebounceMs: 300,
+    refreshOnEvents: [],
+    filterQueryParamNames: {},
+    pageNumber: 1,
+    itemsPerPage: 5,
+    sortKey: "",
+    sortDirection: "",
+    searchQuery: "",
+    filterValues: {},
+    selectedRowIds: [],
+    isLoading: false,
+    hasRefreshError: false,
+    liveMessage: "",
+    refreshTimeoutId: null,
+    refreshEventHandlers: [],
+    rootElement: null,
+
+    tableRegion() {
+      return this.rootElement.querySelector("[data-ui-data-table]");
+    },
+
+    tableBody() {
+      return this.rootElement.querySelector("tbody");
+    },
+
+    currentPageRowIds() {
+      const rowCheckboxes =
+        this.tableBody()?.querySelectorAll("input[type=checkbox]") ?? [];
+      return Array.from(rowCheckboxes).map((checkbox) => checkbox.value);
+    },
+
+    isCurrentPageFullySelected() {
+      const pageRowIds = this.currentPageRowIds();
+      return (
+        pageRowIds.length > 0 &&
+        pageRowIds.every((rowId) => this.selectedRowIds.includes(rowId))
+      );
+    },
+
+    isCurrentPagePartiallySelected() {
+      const pageRowIds = this.currentPageRowIds();
+      const selectedCount = pageRowIds.filter((rowId) =>
+        this.selectedRowIds.includes(rowId),
+      ).length;
+      return selectedCount > 0 && selectedCount < pageRowIds.length;
+    },
+
+    toggleCurrentPageSelection() {
+      const pageRowIds = this.currentPageRowIds();
+      if (this.isCurrentPageFullySelected()) {
+        this.selectedRowIds = this.selectedRowIds.filter(
+          (rowId) => !pageRowIds.includes(rowId),
+        );
+        return;
+      }
+      this.selectedRowIds = [
+        ...new Set([...this.selectedRowIds, ...pageRowIds]),
+      ];
+    },
+
+    ariaSortFor(columnSortKey) {
+      if (this.sortKey !== columnSortKey) {
+        return "none";
+      }
+      if (this.sortDirection === "asc") {
+        return "ascending";
+      }
+      if (this.sortDirection === "desc") {
+        return "descending";
+      }
+      return "none";
+    },
+
+    sortIconClassFor(columnSortKey) {
+      if (this.sortKey !== columnSortKey) {
+        return unsortedSortIconClass;
+      }
+      if (this.sortDirection === "asc") {
+        return "ph-caret-up";
+      }
+      if (this.sortDirection === "desc") {
+        return "ph-caret-down";
+      }
+      return unsortedSortIconClass;
+    },
+
+    placeholderValue(placeholderName) {
+      switch (placeholderName) {
+        case "pageNumber":
+          return String(this.pageNumber);
+        case "itemsPerPage":
+          return String(this.itemsPerPage);
+        case "sortKey":
+          return this.sortKey;
+        case "sortDirection":
+          return this.sortDirection;
+        case "search":
+          return this.searchQuery;
+        default:
+          return "";
+      }
+    },
+
+    resolveTemplatePair(queryPair) {
+      const separatorIndex = queryPair.indexOf("=");
+      if (separatorIndex === -1) {
+        return queryPair;
+      }
+      const pairValue = queryPair.slice(separatorIndex + 1);
+      const placeholderPattern =
+        /^\{(pageNumber|itemsPerPage|sortKey|sortDirection|search)\}$/;
+      const placeholderMatch = pairValue.match(placeholderPattern);
+      if (!placeholderMatch) {
+        return queryPair;
+      }
+      const placeholderValue = this.placeholderValue(placeholderMatch[1]);
+      if (placeholderValue === "") {
+        return null;
+      }
+      const pairKey = queryPair.slice(0, separatorIndex + 1);
+      return pairKey + encodeURIComponent(placeholderValue);
+    },
+
+    buildFilterPairs() {
+      const filterPairs = [];
+      for (const [filterKey, filterValue] of Object.entries(
+        this.filterValues ?? {},
+      )) {
+        const queryParamName =
+          this.filterQueryParamNames[filterKey] ?? filterKey;
+        const encodedQueryParamName = encodeURIComponent(queryParamName);
+        if (filterValue && typeof filterValue === "object") {
+          if (filterValue.min) {
+            const encodedMin = encodeURIComponent(filterValue.min);
+            filterPairs.push(`${encodedQueryParamName}Min=${encodedMin}`);
+          }
+          if (filterValue.max) {
+            const encodedMax = encodeURIComponent(filterValue.max);
+            filterPairs.push(`${encodedQueryParamName}Max=${encodedMax}`);
+          }
+          continue;
+        }
+        if (
+          filterValue === null ||
+          filterValue === undefined ||
+          filterValue === ""
+        ) {
+          continue;
+        }
+        const encodedValue = encodeURIComponent(filterValue);
+        filterPairs.push(`${encodedQueryParamName}=${encodedValue}`);
+      }
+      return filterPairs;
+    },
+
+    buildRefreshUrl() {
+      const [baseUrl, queryString = ""] = this.queryUrlTemplate.split("?");
+      const queryPairs = queryString ? queryString.split("&") : [];
+      const resolvedPairs = queryPairs
+        .map((queryPair) => this.resolveTemplatePair(queryPair))
+        .filter((queryPair) => queryPair !== null);
+      const allPairs = [...resolvedPairs, ...this.buildFilterPairs()];
+      if (allPairs.length === 0) {
+        return baseUrl;
+      }
+      return `${baseUrl}?${allPairs.join("&")}`;
+    },
+
+    async fetchTableRegion(refreshUrl) {
+      const response = await fetch(refreshUrl, {
+        headers: { "HX-Request": "true" },
+      });
+      if (!response.ok) {
+        throw new Error(`HTTP ${response.status}`);
+      }
+      const responseText = await response.text();
+      const responseDocument = new DOMParser().parseFromString(
+        responseText,
+        "text/html",
+      );
+      const freshTableRegion = responseDocument.querySelector(
+        "[data-ui-data-table]",
+      );
+      if (!freshTableRegion) {
+        throw new Error("Response is missing the data-ui-data-table fragment");
+      }
+      this.tableRegion().replaceWith(freshTableRegion);
+    },
+
+    async refresh() {
+      if (!this.queryUrlTemplate) {
+        return;
+      }
+      clearTimeout(this.refreshTimeoutId);
+      this.isLoading = true;
+      this.hasRefreshError = false;
+      const refreshUrl = this.buildRefreshUrl();
+
+      try {
+        if (window.htmx?.ajax) {
+          await window.htmx.ajax("GET", refreshUrl, {
+            source: this.tableRegion(),
+            target: this.tableRegion(),
+            select: "[data-ui-data-table]",
+            swap: "outerHTML",
+          });
+          if (!this.hasRefreshError) {
+            this.liveMessage = "Table refreshed";
+          }
+          return;
+        }
+        await this.fetchTableRegion(refreshUrl);
+        this.liveMessage = "Table refreshed";
+      } catch (refreshError) {
+        this.hasRefreshError = true;
+        console.error(`DataTableRefreshFailed: ${refreshError.message}`);
+      } finally {
+        this.isLoading = false;
+      }
+    },
+
+    requestRefresh() {
+      clearTimeout(this.refreshTimeoutId);
+      this.refreshTimeoutId = setTimeout(
+        () => this.refresh(),
+        this.refreshDebounceMs,
+      );
+    },
+
+    resetPageAndRefresh() {
+      this.pageNumber = 1;
+      this.requestRefresh();
+    },
+
+    toggleSort(columnSortKey) {
+      this.pageNumber = 1;
+      if (this.sortKey !== columnSortKey) {
+        this.sortKey = columnSortKey;
+        this.sortDirection = "asc";
+        this.requestRefresh();
+        return;
+      }
+      if (this.sortDirection === "asc") {
+        this.sortDirection = "desc";
+        this.requestRefresh();
+        return;
+      }
+      this.sortKey = "";
+      this.sortDirection = "";
+      this.requestRefresh();
+    },
+
+    destroy() {
+      for (const { refreshEventName, refreshEventHandler } of this
+        .refreshEventHandlers) {
+        window.removeEventListener(refreshEventName, refreshEventHandler);
+      }
+      this.rootElement.removeEventListener(
+        "htmx:responseError",
+        this.htmxResponseErrorHandler,
+      );
+      this.rootElement.removeEventListener(
+        "htmx:sendError",
+        this.htmxResponseErrorHandler,
+      );
+      clearTimeout(this.refreshTimeoutId);
+    },
+
+    init() {
+      this.rootElement = this.$el;
+      const settingsElement = document.getElementById(settingsScriptId);
+      if (!settingsElement) {
+        console.error(`DataTableSettingsScriptMissing: ${settingsScriptId}`);
+        return;
+      }
+
+      let clientSettings;
+      try {
+        clientSettings = JSON.parse(settingsElement.textContent);
+      } catch (parseError) {
+        console.error(`DataTableSettingsParseFailed: ${parseError.message}`);
+        return;
+      }
+
+      this.queryUrlTemplate = clientSettings.queryUrlTemplate || "";
+      this.refreshDebounceMs = clientSettings.refreshDebounceMs || 300;
+      this.refreshOnEvents = clientSettings.refreshOnEvents || [];
+      this.filterQueryParamNames = clientSettings.filterQueryParamNames || {};
+
+      const initialState = clientSettings.initialState || {};
+      this.pageNumber = initialState.pageNumber ?? 1;
+      this.itemsPerPage = initialState.itemsPerPage ?? 5;
+      this.sortKey = initialState.sortKey ?? "";
+      this.sortDirection = initialState.sortDirection ?? "";
+      this.searchQuery = initialState.searchQuery ?? "";
+      this.filterValues = initialState.filterValues ?? {};
+
+      for (const refreshEventName of this.refreshOnEvents) {
+        const refreshEventHandler = () => this.requestRefresh();
+        this.refreshEventHandlers.push({
+          refreshEventName,
+          refreshEventHandler,
+        });
+        window.addEventListener(refreshEventName, refreshEventHandler);
+      }
+
+      this.$watch("selectedRowIds", () => {
+        this.liveMessage =
+          this.selectedRowIds.length === 1
+            ? "1 row selected"
+            : `${this.selectedRowIds.length} rows selected`;
+      });
+
+      this.htmxResponseErrorHandler = () => {
+        this.hasRefreshError = true;
+        this.isLoading = false;
+      };
+      this.rootElement.addEventListener(
+        "htmx:responseError",
+        this.htmxResponseErrorHandler,
+      );
+      this.rootElement.addEventListener(
+        "htmx:sendError",
+        this.htmxResponseErrorHandler,
+      );
+    },
+  }));
+});
